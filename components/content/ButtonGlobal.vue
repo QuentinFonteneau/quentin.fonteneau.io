0cfb1cf2da5656a1e0bcf79b75928d928ed3416f
Quentin Fonteneau <qfonteneau@digitalgarden.fr>
2024-09-26 11:05:36 +0200

    Test

---
+++ components/content/ButtonGlobal.vue
@@ -0,0 +1,22 @@
+<!-- components/Button.vue -->
+<template>
+  <button class="btn">{{ label }}</button>
+</template>
+
+<script setup>
+  defineProps({
+    label: {
+      type: String,
+      required: true,
+    },
+  });
+</script>
+
+<style scoped>
+  .btn {
+    background-color: #007bff;
+    color: white;
+    padding: 10px;
+    border-radius: 5px;
+  }
+</style>
