320dcd64e8af00a86db42c1f2ab639465cc9a7c5
Quentin Fonteneau <qfonteneau@digitalgarden.fr>
2024-11-04 16:42:37 +0100

    Use linkedin.svg

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,7 +4,7 @@
       <Logo />
       <img
         loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/8e8f3c76ca6cf5f37bc41cfb8898871ccb216af4c9fbea8b4b175b0c8b8ea721?apiKey=b0cab2777511409392181b9d50ff6276&"
+        src="/img/linkedin.svg"
         class="linkedin" />
     </div>
     <div class="footer__second">
