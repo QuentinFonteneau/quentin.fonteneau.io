70065cc72be0696cd3a5400be3565a6cfd9f47d9
Quentin Fonteneau <quentin@fonteneau.io>
2024-12-11 10:59:23 +0100

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Variables
+# Variables communes
 LOCAL_BUILD_DIR=".output/public"
 REMOTE_USER="root"
-REMOTE_HOST="89.117.50.91"
-REMOTE_DIR="/home/quentin/new.quentin.fonteneau.io/quentin.fonteneau.io/"
+
+# Vérification du paramètre passé au script
+if [ "$1" == "prod" ]; then
+  REMOTE_HOST="89.117.50.91"
+  REMOTE_DIR="/home/quentin/prod.quentin.fonteneau.io/quentin.fonteneau.io/"
+elif [ "$1" == "preprod" ]; then
+  REMOTE_HOST="89.117.50.91"
+  REMOTE_DIR="/home/quentin/preprod.quentin.fonteneau.io/quentin.fonteneau.io/"
+else
+  echo "Usage: $0 [prod|preprod]"
+  exit 1
+fi
 
 # Commande rsync pour déployer uniquement les différences
 rsync -avz --delete --exclude=".htaccess" --exclude=".htpasswd" "$LOCAL_BUILD_DIR/" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
@@ -17,4 +27,4 @@ rsync -avz --delete --exclude=".htaccess" --exclude=".htpasswd" "$LOCAL_BUILD_DI
 # --exclude=".htaccess" : exclut le fichier .htaccess de la suppression
 # --exclude=".htpasswd" : exclut le fichier .htpasswd de la suppression
 
-echo "Déploiement terminé !"
+echo "Déploiement terminé sur l'environnement $1 !"
